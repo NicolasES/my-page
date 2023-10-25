7740e2229d639910d33c2a1e79f45db1b6132128
NicolasES <nicolas.escouto@gmail.com>
2023-10-25 16:01:53 -0300

    feat: pdf curriculo

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -1,9 +1,10 @@
-import { Box, Container, Link, Typography } from "@mui/material"
+import { Box, Container, Link, Tooltip, Typography } from "@mui/material"
 import Grid from '@mui/material/Unstable_Grid2' // Grid version 2
 import { MyImage, TitleDivider } from "../shared"
 import meImg from '../../assets/me.jpeg'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'
 
 
 export const AboutMe: React.FC = () => {
@@ -35,18 +36,30 @@ export const AboutMe: React.FC = () => {
           <Typography variant="body1" sx={styles.p} mt={3}>
             Tenho grande experiência na criação de APIs rest. Atualmente utilizo
             NodeJS / Typescript como principal tecnologia de desenvolvimento.
-            Gosto de participar da arquitetura do sistema, já trabalhei com Clean Architecture na criação de microsserviços, 
+            Gosto de participar da arquitetura do sistema, já trabalhei com Clean Architecture na criação de microsserviços,
             testes com Jest e TDD,
             Mensageria Kafka e SQS.
           </Typography>
 
-          <Box mt={3}>
-            <Link href="https://www.linkedin.com/in/nicolas-escouto-sonnenfeld-42b17298/" target="_blank" color="inherit">
-              <LinkedInIcon fontSize="large" sx={{ mx: 3 }}></LinkedInIcon>
-            </Link>
-            <Link href="https://github.com/NicolasES" target="_blank" color="inherit">
-              <GitHubIcon fontSize="large" sx={{ mx: 3 }}></GitHubIcon>
-            </Link>
+          <Box mt={3} alignItems='center' display='flex'>
+
+            <Tooltip title="LinkedIn">
+              <Link href="https://www.linkedin.com/in/nicolas-escouto-sonnenfeld-42b17298/" target="_blank" color="inherit">
+                <LinkedInIcon fontSize="large" sx={{ mx: 3 }}></LinkedInIcon>
+              </Link>
+            </Tooltip>
+
+            <Tooltip title="GitHub">
+              <Link href="https://github.com/NicolasES" target="_blank" color="inherit">
+                <GitHubIcon fontSize="large" sx={{ mx: 3 }}></GitHubIcon>
+              </Link>
+            </Tooltip>
+
+            <Tooltip title="Currículo em PDF">
+              <Link href="./Currículo-NICOLAS.pdf" target="_blank" color="inherit">
+                <PictureAsPdfIcon fontSize="large" sx={{ mx: 3 }}></PictureAsPdfIcon>
+              </Link>
+            </Tooltip>
           </Box>
         </Grid>
       </Grid>
